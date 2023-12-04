d814de7996671acaebe8926e39b8f4a34a64e2e7
Schwer <42115696+itsschwer@users.noreply.github.com>
2023-12-05 10:36:40 +1100

    Implement timed pressure plates

--- PressYourPlate/Plugin.cs
+++ PressYourPlate/Plugin.cs
@@ -1,5 +1,7 @@
 using BepInEx;
 using RoR2;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace PressYourPlate
@@ -18,7 +20,8 @@ namespace PressYourPlate
         public const string Name = "PressYourPlate";
         public const string Version = "0.0.0";
 
-        // The Awake() method is run at the very start when the game is initialized.
+        private static Dictionary<PressurePlateController, float> timers = new(2);
+
         private void Awake() => Log.Init(Logger);
 
         private void OnEnable()
@@ -41,20 +44,54 @@ namespace PressYourPlate
 #endif
         }
 
+        private void Update()
+        {
+            foreach (PressurePlateController key in timers.Keys.ToList()) {
+                timers[key] -= Time.deltaTime;
+                if (timers[key] <= 0) {
+                    timers.Remove(key);
+
+#if DEBUG
+                    string identifier = $"[no longer exists.]";
+                    if (key) identifier = $"{key.name} @ {key.transform.position}";
+                    Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = $"<style=cIsUtility>> {identifier} inactive ({timers.Count} active)</style>" });
+#endif
+                }
+            }
+        }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("", "Publicizer001")]
         private static void PermaPressPressurePlate(On.RoR2.PressurePlateController.orig_SetSwitch orig, PressurePlateController self, bool switchIsDown)
         {
-            // Only call original function when pressure plate is being pressed -- pressure plate will never release
+            // todo: make configurable -- <0 = perma, 0 = off, >0 = timed
+            const float time = 30f;
+
             if (switchIsDown) {
                 if (switchIsDown != self.switchDown) {
                     string message = (Random.value <= 0.2) ? "Press your plate!" : "A pressure plate is pressed..";
-                    Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = "<style=cUserSetting>" + message + "</style>" });
+                    Output(message);
+                }
+
+                timers[self] = time;
+                orig(self, switchIsDown);
+
+#if DEBUG
+                string identifier = $"[no longer exists?]";
+                if (self) identifier = $"{self.name} @ {self.transform.position}";
+                Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = $"<style=cIsUtility>> {identifier} active {time}s ({timers.Count} active)</style>" });
+#endif
+            }
+            else if (time > 0 && !timers.ContainsKey(self)) {
+                if (switchIsDown != self.switchDown) {
+                    Output("A pressure plate releases...");
                 }
 
                 orig(self, switchIsDown);
             }
+        }
 
-            // Would ideally replace with a configurable timer for how long a pressure plate remains depressed
+        private static void Output(string message) {
+            Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = "<style=cStack>" + message + "</style>" });
         }
     }
 }
