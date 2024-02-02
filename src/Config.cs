8fb7477a790bb9cbf39ff8a37495ebed064150be
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-02-02 15:42:27 +1100

    Refactor Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -4,100 +4,107 @@ namespace PressureDrop
 {
     public sealed class Config
     {
-        private readonly ConfigEntry<float> _pressurePlateTimer;
-        public float PressurePlateTimer => _pressurePlateTimer.Value;
+        private readonly ConfigEntry<float> pressurePlateTimer;
+        public float PressurePlateTimer => pressurePlateTimer.Value;
 
 
         // Drop Command
-        private readonly ConfigEntry<bool> _dropEnabled;
-        private readonly ConfigEntry<bool> _dropTeleporterEnabled;
-        private readonly ConfigEntry<bool> _dropDeadEnabled;
-        private readonly ConfigEntry<bool> _dropInvertDirection;
-        private readonly ConfigEntry<bool> _dropVoidAllowed;
-        private readonly ConfigEntry<int> _maxItemsToDropAtATime;
+        private readonly ConfigEntry<bool> dropEnabled;
+        private readonly ConfigEntry<bool> dropTeleporterEnabled;
+        private readonly ConfigEntry<bool> dropDeadEnabled;
+        private readonly ConfigEntry<bool> dropInvertDirection;
+        private readonly ConfigEntry<bool> dropVoidAllowed;
+        private readonly ConfigEntry<int> maxItemsToDropAtATime;
         // Accessors
-        public bool DropEnabled => _dropEnabled.Value;
-        public bool DropTeleporterEnabled => _dropTeleporterEnabled.Value;
-        public bool DropDeadEnabled => _dropDeadEnabled.Value;
-        public bool DropInvertDirection => _dropInvertDirection.Value;
-        public bool DropVoidAllowed => _dropVoidAllowed.Value;
-        public int MaxItemsToDropAtATime => _maxItemsToDropAtATime.Value > 0 ? _maxItemsToDropAtATime.Value : 1;
+        public bool DropEnabled => dropEnabled.Value;
+        public bool DropTeleporterEnabled => dropTeleporterEnabled.Value;
+        public bool DropDeadEnabled => dropDeadEnabled.Value;
+        public bool DropInvertDirection => dropInvertDirection.Value;
+        public bool DropVoidAllowed => dropVoidAllowed.Value;
+        public int MaxItemsToDropAtATime => maxItemsToDropAtATime.Value > 0 ? maxItemsToDropAtATime.Value : 1;
 
 
         // Drop Recyclable
-        private readonly ConfigEntry<bool> _dropRecyclableWhite;
-        private readonly ConfigEntry<bool> _dropRecyclableGreen;
-        private readonly ConfigEntry<bool> _dropRecyclableRed;
-        private readonly ConfigEntry<bool> _dropRecyclableYellow;
-        private readonly ConfigEntry<bool> _dropRecyclableLunar;
-        private readonly ConfigEntry<bool> _dropRecyclableVoid;
-        private readonly ConfigEntry<bool> _dropRecyclableEquipment;
-        private readonly ConfigEntry<bool> _dropRecyclableEquipmentLunar;
+        private readonly ConfigEntry<bool> dropRecyclableWhite;
+        private readonly ConfigEntry<bool> dropRecyclableGreen;
+        private readonly ConfigEntry<bool> dropRecyclableRed;
+        private readonly ConfigEntry<bool> dropRecyclableYellow;
+        private readonly ConfigEntry<bool> dropRecyclableLunar;
+        private readonly ConfigEntry<bool> dropRecyclableVoid;
+        private readonly ConfigEntry<bool> dropRecyclableEquipment;
+        private readonly ConfigEntry<bool> dropRecyclableEquipmentLunar;
         // Accessors
-        public bool DropRecyclableWhite => _dropRecyclableWhite.Value;
-        public bool DropRecyclableGreen => _dropRecyclableGreen.Value;
-        public bool DropRecyclableRed => _dropRecyclableRed.Value;
-        public bool DropRecyclableYellow => _dropRecyclableYellow.Value;
-        public bool DropRecyclableLunar => _dropRecyclableLunar.Value;
-        public bool DropRecyclableVoid => _dropRecyclableVoid.Value;
-        public bool DropRecyclableEquipment => _dropRecyclableEquipment.Value;
-        public bool DropRecyclableEquipmentLunar => _dropRecyclableEquipmentLunar.Value;
+        public bool DropRecyclableWhite => dropRecyclableWhite.Value;
+        public bool DropRecyclableGreen => dropRecyclableGreen.Value;
+        public bool DropRecyclableRed => dropRecyclableRed.Value;
+        public bool DropRecyclableYellow => dropRecyclableYellow.Value;
+        public bool DropRecyclableLunar => dropRecyclableLunar.Value;
+        public bool DropRecyclableVoid => dropRecyclableVoid.Value;
+        public bool DropRecyclableEquipment => dropRecyclableEquipment.Value;
+        public bool DropRecyclableEquipmentLunar => dropRecyclableEquipmentLunar.Value;
 
 
         // Tweaks
-        private readonly ConfigEntry<bool> _voidPickupConfirmAll;
-        private readonly ConfigEntry<bool> _voidFieldFogAltStart;
+        private readonly ConfigEntry<bool> voidPickupConfirmAll;
+        private readonly ConfigEntry<bool> voidFieldFogAltStart;
         // Accessors
-        public bool VoidPickupConfirmAll => _voidPickupConfirmAll.Value;
-        public bool VoidFieldFogAltStart => _voidFieldFogAltStart.Value;
+        public bool VoidPickupConfirmAll => voidPickupConfirmAll.Value;
+        public bool VoidFieldFogAltStart => voidFieldFogAltStart.Value;
 
 
         public Config(ConfigFile config)
         {
             // Prefix section to sort to top in config editor
-            _pressurePlateTimer = config.Bind<float>("> Timed Pressure Plates", "pressurePlateTimer", 30f,
+            config.Bind<float>("> Timed Pressure Plates", ref pressurePlateTimer, 30f,
                 "The length of time (seconds) a pressure plate will remain pressed after being activated.\nZero disables time functionality (reverts to vanilla behaviour). Negative values prevent pressure plates from releasing once activated.");
 
 
             const string DropCommand = "Drop Command";
-            _dropEnabled = config.Bind<bool>(DropCommand, "dropEnabled", true,
+            config.Bind<bool>(DropCommand, ref dropEnabled, true,
                 "Whether the /drop command should be enabled or not.");
-            _dropTeleporterEnabled = config.Bind<bool>(DropCommand, "dropTeleporterEnabled", true,
+            config.Bind<bool>(DropCommand, ref dropTeleporterEnabled, true,
                 "Whether players should be able to send their drops to the Teleporter or not.");
-            _dropDeadEnabled = config.Bind<bool>(DropCommand, "dropDeadEnabled", true,
+            config.Bind<bool>(DropCommand, ref dropDeadEnabled, true,
                 "Whether dead players should be able to drop items or not.\nRequires dropTeleporterEnabled to be true.");
-            _dropInvertDirection = config.Bind<bool>(DropCommand, "dropInvertDirection", false,
+            config.Bind<bool>(DropCommand, ref dropInvertDirection, false,
                 "Whether items should be dropped opposite the aim direction or not.");
-            _dropVoidAllowed = config.Bind<bool>(DropCommand, "dropVoidAllowed", false,
+            config.Bind<bool>(DropCommand, ref dropVoidAllowed, false,
                 "Whether void items are allowed to be dropped or not.");
-            _maxItemsToDropAtATime = config.Bind<int>(DropCommand, "maxItemsToDropAtATime", 10,
+            config.Bind<int>(DropCommand, ref maxItemsToDropAtATime, 10,
                 "The maximum amount of items to drop from the player at a time (similar to Scrappers).\nMinimum value is 1.");
 
 
             const string DropRecyclable = "Drop Recyclable";
-            _dropRecyclableWhite = config.Bind<bool>(DropRecyclable, "dropRecyclableWhite", true,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableWhite, true,
                 "Whether dropped white tier items should be recyclable.");
-            _dropRecyclableGreen = config.Bind<bool>(DropRecyclable, "dropRecyclableGreen", true,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableGreen, true,
                 "Whether dropped green tier items should be recyclable.");
-            _dropRecyclableRed = config.Bind<bool>(DropRecyclable, "dropRecyclableRed", false,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableRed, false,
                 "Whether dropped red tier items should be recyclable.");
-            _dropRecyclableYellow = config.Bind<bool>(DropRecyclable, "dropRecyclableYellow", false,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableYellow, false,
                 "Whether dropped yellow tier items should be recyclable.");
-            _dropRecyclableLunar = config.Bind<bool>(DropRecyclable, "dropRecyclableLunar", false,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableLunar, false,
                 "Whether dropped lunar items should be recyclable.");
-            _dropRecyclableVoid = config.Bind<bool>(DropRecyclable, "dropRecyclableVoid", false,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableVoid, false,
                 "Whether dropped void items should be recyclable.");
-            _dropRecyclableEquipment = config.Bind<bool>(DropRecyclable, "dropRecyclableEquipment", true,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableEquipment, true,
                 "Whether dropped equipment should be recyclable.");
-            _dropRecyclableEquipmentLunar = config.Bind<bool>(DropRecyclable, "dropRecyclableEquipmentLunar", false,
+            config.Bind<bool>(DropRecyclable, ref dropRecyclableEquipmentLunar, false,
                 "Whether dropped lunar equipment should be recyclable.");
 
 
             const string Tweaks = "Tweaks";
-            _voidPickupConfirmAll = config.Bind<bool>(Tweaks, "voidPickupConfirmAll", false,
+            config.Bind<bool>(Tweaks, ref voidPickupConfirmAll, false,
                 "Whether void items always need to picked up intentionally via interact prompt.");
-            _voidFieldFogAltStart = config.Bind<bool>(Tweaks, "voidFieldFogAltStart", false,
+            config.Bind<bool>(Tweaks, ref voidFieldFogAltStart, false,
                 "Whether the Void Fog should only become active once a Cell Vent has been activated.");
+        }
+    }
+
+    internal static class ConfigUtility
+    {
+        public static void Bind<T>(this ConfigFile config, string section, ref ConfigEntry<T> key, T defaultValue, string description) {
+            key = config.Bind(section, nameof(key), defaultValue, description);
         }
     }
 }
